16ee06a5c0145376e8ab43e92ef8738d3e0a2acb
Fleych <nikita17770@gmail.com>
2025-05-23 06:41:32 +0700

    Add files via upload

--- Interpreter/Program.cs
+++ Interpreter/Program.cs
@@ -1,14 +1,44 @@
 ﻿using Interpreter;
 
-LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer();
+Storage storage = new Storage();
+Lexer lexer = new Lexer(storage);
+Parser parser = new Parser(storage);
 
-string program = "int a1 = 10;\nif (a1 >= 0) {write(a1 + 7);} else a1 = 0;";
+Console.WriteLine("Лексический анализатор\n");
 
-lexicalAnalyzer.Analyze(program);
+/*string program = "{\n" +
+                      "int[3] vec;\n" +
+                      "int a;\n" +
+                      "vec[0] = 2; vec[1] = 5; vec[2] = 7; a = 0;\n" +
+                      "while (a < vec[2]) {a = a + 1;}\n" +
+                      "if (a != vec[0])\n" +
+                      "{\n" +
+                      "    write(a);\n" +
+                      "}\n" +
+                      "else\n" +
+                      "{\n" +
+                      "    a = a * -1;\n"+
+                      "}\n" + 
+                  "}"; // <- ДЕНЧИК ЭТО РАБОТАЕТ*/
 
-Console.WriteLine("Номер: " + "Лексема:");
+string program = "{int a;}";
 
-for (int i = 0; i < lexicalAnalyzer.lexeme_code.Count; i++)
+lexer.Analyze(program);
+
+Console.WriteLine("Номер " + "Лексема " + "Строка:символ");
+
+for (int i = 0; i < storage.lexems.Count; i++)
 {
-    Console.WriteLine(lexicalAnalyzer.lexeme_code[i].ToString() + " " + lexicalAnalyzer.lexeme_str[i]);
+    Console.WriteLine(storage.lexems[i].ToString());
+}
+
+parser.Analyze();
+
+Console.WriteLine("\nСинтаксический анализатор\n");
+
+Console.WriteLine("Номер: " + "ОПС: " + "     ТИП:");
+
+for (int i = 0; i < storage.RPN.Count; i++)
+{
+    Console.WriteLine(i.ToString() + "      " + storage.RPN[i] + "      " + storage.RPN_types[i]);
 }
